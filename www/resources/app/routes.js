a297bebf476a43a1e961cfdbec8eb2c3eff99b15
DimiUA <s.dimi.d@gmil.com>
2020-04-15 00:16:48 +0300

    asasas

--- www/resources/app/routes.js
+++ www/resources/app/routes.js
@@ -31,6 +31,32 @@ var routes = [
         componentUrl: './resources/pages/share.html?v=2.1',
         name: 'share',
     },
+    {
+        path: '/inform-about-test/',
+        componentUrl: './resources/pages/inform.about.test.html?v=2.1',
+        name: 'inform.about.test',
+    },
+    {
+        path: '/inform-about-recovery/',
+        componentUrl: './resources/pages/inform.about.recovery.html?v=2.1',
+        name: 'inform.about.recovery',
+    },
+    {
+        path: '/qr-code/',
+        componentUrl: './resources/pages/qr.code.html?v=2.1',
+        name: 'qr.code',
+    },
+
+    {
+        path: '/registration/',
+        componentUrl: './resources/pages/registration.html?v=2.1',
+        name: 'registration',
+    },
+    {
+        path: '/notifications/',
+        componentUrl: './resources/pages/notifications.html?v=2.1',
+        name: 'notifications',
+    },
 
     // Default route (404 page). MUST BE THE LAST
     {
